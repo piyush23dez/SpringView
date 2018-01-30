72208e59f0fe4f4b0c1a847d5386a4052a69b3a7
Piyush Sharma <piyush_sharma2@apple.com>
2018-01-30 10:59:38 -0800

    stackoverflow

--- RoundedButton/ViewController.swift
+++ RoundedButton/ViewController.swift
@@ -134,3 +134,94 @@ class SpringView: UIView {
     }
 }
 
+@IBDesignable
+class CurtainView: UIView {
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+
+        let path = UIBezierPath()
+
+        let p0 = CGPoint(x: 0, y: 0)
+        let p1 = CGPoint(x: bounds.size.width, y: 0)
+        let p2 = CGPoint(x: bounds.size.width, y: bounds.size.height-35)
+        let p3 = CGPoint(x: 0, y: bounds.size.height-35)
+        let controlPoint = CGPoint(x: bounds.size.width/2, y: bounds.size.height+10)
+
+        path.move(to: p0)
+        path.addLine(to: p1)
+        path.addLine(to: p2)
+        path.addQuadCurve(to: p3, controlPoint: controlPoint)
+        path.close()
+
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.fillColor = UIColor.orange.withAlphaComponent(0.7).cgColor
+        layer.insertSublayer(shape, at: 0)
+    }
+}
+
+
+/*  When we implement -drawRect:, the background color of our view is then drawn into the associated CALayer, rather than just being set on the CALayer as a style property... thus prevents it from getting a contents animated, The solution is to add another view above, behind, or wherever, and animate that. This animates just fine.
+ 
+    we can apply layer effects to our mask object and it will work just fine. Just don't forget to set to mask a
+    color, any color, as long as you have alpha value greater than 0.0.
+    http://arsenkin.com/ios-mask-view-with-view.html
+*/
+
+@IBDesignable
+class CurtainSpring: UIView {
+    
+    let curtain = CurtainView() //is going to be animated from top to bottom
+    let shape = UIView() //is going to mask everything with alpha mask
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        self.backgroundColor = UIColor.yellow
+        self.shape.backgroundColor = UIColor.orange
+        self.curtain.backgroundColor = .clear
+        
+        //added our curtain view to animate
+        self.addSubview(curtain)
+        self.mask = shape
+        reset()
+    }
+    
+    override func didMoveToSuperview() {
+        animate()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        curtain.frame = self.bounds
+        shape.frame = self.bounds
+    }
+    
+    func reset() {
+        curtain.frame.origin.y = 0
+    }
+    
+    func animate() {
+        reset()
+        
+        UIView.animate(withDuration: 1, animations: {
+            self.curtain.frame.origin.y = self.frame.size.height
+        }) { (finish) in
+            
+            UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+                self.curtain.frame.origin.y -= 15
+            }, completion: { (f) in })
+        }
+    }
+}
